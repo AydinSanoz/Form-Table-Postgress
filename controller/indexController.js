4b67fb0c6bbeceedc909d5123ca3f7d6d2e6aa48
E2029-Aydin <aydinsanoz2@gmail.com>
2020-12-21 00:39:04 +0300

    index controller added

---
+++ controller/indexController.js
@@ -0,0 +1,3 @@
+exports.show_index = (req, res, next) => {
+	res.render('index', { title: 'Express jvnxclkvnlxc' });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var indexController = require('../controller/indexController')
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.show_index);
 
 module.exports = router;
